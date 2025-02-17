305cdd53797352da0d32d9d6320a85fcae6957b4
unknown <riverplant@hotmail.com>
2025-02-17 15:28:32 -0500

    Reinitialize

---
+++ src/main/java/com/riverplant/mscloud/MscloudApplication.java
@@ -0,0 +1,13 @@
+package com.riverplant.mscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riverplant/mscloud/MscloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riverplant.mscloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
