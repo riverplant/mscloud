8140fb31ddaff6e254284ff1fd37b0554407efa0
unknown <riverplant@hotmail.com>
2025-02-17 15:53:54 -0500

    test

--- src/main/java/com/riverplant/mscloud/MscloudApplication.java
+++ src/main/java/com/riverplant/mscloud/MscloudApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MscloudApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(MscloudApplication.class, args);
 	}
 
